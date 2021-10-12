2de8c31345ef8e503a99a9ce39af17d0ab1b1ae6
nilberthsouza <nilberth@protonmail.com>
2021-10-12 12:50:17 -0300

    #define, ifdelf, else,endif - adicionados

--- quinze.c
+++ quinze.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+#define MASSATERRA  5.9e+24
+#define G  6.7e-11
+
 int main(void){
 
-    double massaTerra = 5.9e+24;
-    double G = 6.7e-11;
     double periodo,raio,velocidade;
 
+    #ifdef MASSATERRA
+    
     printf("Insira o raio da orbita circular do satelite");
     scanf("%lf",&raio);
 
-    velocidade = sqrt(G*massaTerra/raio);
+    velocidade = sqrt(G*MASSATERRA/raio);
 
-    periodo = 2 * M_PI * sqrt(pow(raio,3)/(G*massaTerra));
+    periodo = 2 * M_PI * sqrt(pow(raio,3)/(G*MASSATERRA));
 
     printf("A velocidade escalar do satelite é: %e", velocidade);
 
     printf("\nA o periodo do movimento orbital é: %e",periodo);
+
+    #else
+    printf("Massa Terra não definida");
+
+    #endif
+    
 
     return 0;
 
